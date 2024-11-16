03fdd079bd5e125b4c6a6a07ae4c129facae3571
TIOTE Sindou <sindoti16@gmail.com>
2024-11-16 12:27:25 +0000

    Création Classe TypeEnquete dans le Package ci.models

--- src/App.java
+++ src/App.java
@@ -1,6 +1,11 @@
+import ci.models.TypeEnquete;
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+
+        TypeEnquete enquete1 = new TypeEnquete(01, "Insertion", "Transversale");
+        enquete1.afficher();
 
         }
     }
---
+++ src/ci/models/TypeEnquete.java
@@ -0,0 +1,42 @@
+package ci.models;
+
+public class TypeEnquete {
+    private int id;
+    private String code;
+    private String libelle;
+    
+    public TypeEnquete(int id, String code, String libelle) {
+        this.id = id;
+        this.code = code;
+        this.libelle = libelle;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getLibelle() {
+        return libelle;
+    }
+
+    public void setLibelle(String libelle) {
+        this.libelle = libelle;
+    }
+
+    public void afficher() {
+        System.out.println(
+            "Mon enquête est de type " + this.libelle + ", de Id" + this.id + " et de code" + this.code);
+}
+}
