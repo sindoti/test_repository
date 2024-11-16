8ff63777e0112b9f8cfce6c885453a1c28c2bc70
TIOTE Sindou <sindoti16@gmail.com>
2024-11-16 13:08:54 +0000

    Création Classe Enquete dans le Package ci.models

--- src/App.java
+++ src/App.java
@@ -1,11 +1,15 @@
+import ci.models.Enquete;
 import ci.models.TypeEnquete;
 
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
 
-        TypeEnquete enquete1 = new TypeEnquete(01, "Insertion", "Transversale");
-        enquete1.afficher();
+        TypeEnquete typenquete1 = new TypeEnquete(01, "Insertion", "Transversale");
+        typenquete1.afficher();
 
+        Enquete enquete1 = new Enquete(1, "desFormes", "se situe", typenquete1);
+
+        enquete1.afficher2();
         }
     }
---
+++ src/ci/models/Enquete.java
@@ -0,0 +1,54 @@
+package ci.models;
+
+public class Enquete {
+
+    private int id;
+    private String titre;
+    private String description;
+    private TypeEnquete typeEnquete;
+    
+    public Enquete(int id, String titre, String description, TypeEnquete typeEnquete) {
+        this.id = id;
+        this.titre = titre;
+        this.description = description;
+        this.typeEnquete = typeEnquete;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitre() {
+        return titre;
+    }
+
+    public void setTitre(String titre) {
+        this.titre = titre;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public TypeEnquete getTypeEnquete() {
+        return typeEnquete;
+    }
+
+    public void setTypeEnquete(TypeEnquete typeEnquete) {
+        this.typeEnquete = typeEnquete;
+    }
+
+    public void afficher2() {
+        System.out.println(
+            "Mon enquête " + this.id + this.titre + " se situe dans le cadre " + this.typeEnquete); 
+}
+}
+
